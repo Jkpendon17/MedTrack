d7f4a3531ae154543e9e928bd9432db1c943a808
Julius Ken Pendon <pendonjulius17@gmail.com>
2026-03-13 00:03:59 +0800

    updated cors error

--- server/server.js
+++ server/server.js
@@ -6,10 +6,6 @@ const db = require("./database");
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
-
-app.use(cors({
-    origin: "https://med-track-git-main-jkpendon17s-projects.vercel.app"
-}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
