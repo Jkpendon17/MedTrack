7d02118904b8a414e03b36f0ea366fd7ab78f8dc
Julius Ken Pendon <pendonjulius17@gmail.com>
2026-03-13 06:36:53 +0800

    trying to fix the cors problem

--- server/server.js
+++ server/server.js
@@ -5,7 +5,12 @@ const db = require("./database");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); 
+app.use(cors({
+    origin:[
+        "https://med-track-git-main-jkpendon17s-projects.vercel.app/",
+        "http://127.0.0.1:5000"
+    ]
+})); 
 app.use(express.json());
 
 app.get("/", (req, res) => {
