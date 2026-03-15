217fd73782afdefe197e6b0f43d4e55cfa13bb72
Julius Ken Pendon <pendonjulius17@gmail.com>
2026-03-15 18:24:49 +0800

    fixing bug in login page

--- client/login.js
+++ client/login.js
@@ -1,3 +1,23 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+
+    console.log(email, pass);
+
+});
+
 function loginBtn(){
     window.location.href ="index.html";
+}
+
+function togglePassword() {
+    const password = document.getElementById("pass");
+
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
 }
