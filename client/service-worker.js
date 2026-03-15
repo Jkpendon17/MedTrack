0548b0a1035bc515eb9bea33984e196735f0db1a
Julius Ken Pendon <pendonjulius17@gmail.com>
2026-03-15 18:00:14 +0800

    Fixing the cache in manifest.json

--- client/service-worker.js
+++ client/service-worker.js
@@ -3,20 +3,33 @@ const CACHE_NAME = "medtracker-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/login.html",
+  "/register.html",
   "/dashboard.html",
-  "/add-medicine.html",
+  "/add_medicine.html",
   "/history.html",
   "/profile.html",
+
   "/manifest.json",
 
   "/style/dashboard.css",
+  "/style/login.css",
+  "/style/register.css",
+  "/style/history.css",
+  "/style/profile.css",
+  "/style/add_medicine.css",
 
-  "/js/dashboard.js",
+  "/dashboard.js",
+  "/login.js",
+  "/register.js",
+  "/history.js",
+  "/profile.js",
+  "/add_medicine.js",
 
   "/assets/favicon.ico",
-  "/assets/app-icon.png"
+  "/assets/app-icon.png",
+  "/assets/profile.png"
 ];
-
 // Install event: save files to cache
 self.addEventListener("install", event => {
   event.waitUntil(
