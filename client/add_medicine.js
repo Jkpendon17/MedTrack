a3df28ddfff72560a7691961a76b41d7ed6fd566
Julius Ken Pendon <pendonjulius17@gmail.com>
2026-03-15 19:12:51 +0800

    fixing the dosage checker

--- client/add_medicine.js
+++ client/add_medicine.js
@@ -1,3 +1,26 @@
+document.getElementById("quantity_taken").addEventListener("input", checkDosage);
+document.getElementById("dosage_per_tablet").addEventListener("input", checkDosage);
+function checkDosage(){
+
+    const qty = document.getElementById("quantity_taken").value;
+    const dosage = document.getElementById("dosage_per_tablet").value;
+
+    const total = qty * dosage;
+
+    document.getElementById("total_dosage").value = total;
+
+    if(total > 2000){
+        document.getElementById("msg").innerText = "⚠ Possible overdose";
+    }else{
+        document.getElementById("msg").innerText = "";
+    }
+
+}
+
+function saveMedicine(){
+    alert("Medicine added!")
+    window.location.href="index.html";
+}
 
 function goBack() {
     window.location.href = "index.html";
