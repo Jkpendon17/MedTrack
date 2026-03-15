d8e08f01e91f12f3fba94fbfb55d235dbf194fd2
Julius Ken Pendon <pendonjulius17@gmail.com>
2026-03-15 18:27:58 +0800

    fixing a LoginBtn

--- client/login.js
+++ client/login.js
@@ -3,13 +3,14 @@ document.getElementById("loginForm").addEventListener("submit", function(e){
 
     const email = document.getElementById("email").value;
     const pass = document.getElementById("pass").value;
-    
-function loginBtn(){
-    window.location.href ="index.html";
-}
+
+
 
 });
 
+function loginBtn(){
+    window.location.href ="index.html";
+}
 
 
 function togglePassword() {
