925a4a77d114634d2918848036048ecfdecab592
Julius Ken Pendon <pendonjulius17@gmail.com>
2026-03-20 05:49:19 +0800

    log in js update

--- client/login.js
+++ client/login.js
@@ -1,13 +1,14 @@
 const form = document.getElementById("loginForm");
 
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const password = document.getElementById("pass").value;
 
 
     // temporary
-    if (email === "test@gmail.com " && password === "1234") {
+    if (email === "test@gmail.com " && pass === "1234") {
         alert("Login successfully");
         window.location.href = "index.html";
     } else {
